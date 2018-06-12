6fe14853210e906cc32d9a151111e3cc686055a8
Carol Lima <carollima.dev@gmail.com>
2018-06-11 23:55:28 -0300

    Project created

---
+++ src/main/java/com/carollima/cmcourse/CmcourseApplication.java
@@ -0,0 +1,12 @@
+package com.carollima.cmcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmcourseApplication.class, args);
+	}
+}
