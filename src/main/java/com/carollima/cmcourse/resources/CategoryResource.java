1cbd3974c94a163e90485f5f1503aa1b5d787804
Carol Lima <carollima.dev@gmail.com>
2018-06-17 23:21:55 -0300

    Testing the first domain class

---
+++ src/main/java/com/carollima/cmcourse/resources/CategoryResource.java
@@ -0,0 +1,31 @@
+package com.carollima.cmcourse.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carollima.cmcourse.domain.Category;
+
+@RestController
+@RequestMapping(value="categories")
+// Rest end point name
+public class CategoryResource {
+	
+	// Http verbs
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<Category>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
+	}
+
+}
