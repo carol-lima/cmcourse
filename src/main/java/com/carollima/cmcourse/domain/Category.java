0bcc467c5c458106bef0a4388baf21e756c6536d
Carol Lima <carollima.dev@gmail.com>
2018-06-19 00:05:45 -0300

    H2 database and automatic database creation

--- src/main/java/com/carollima/cmcourse/domain/Category.java
+++ src/main/java/com/carollima/cmcourse/domain/Category.java
@@ -1,8 +1,16 @@
 package com.carollima.cmcourse.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 	private static final long serialVersioUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 
